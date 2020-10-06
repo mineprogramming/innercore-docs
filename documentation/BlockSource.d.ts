a705918334a8ee02263e16e0294cda74a3e2704e
Саня Череп <41489405+SDesya74@users.noreply.github.com>
2020-10-06 10:24:48 +0300

    Add BlockSource declarations (without description)

---
+++ documentation/BlockSource.d.ts
@@ -0,0 +1,19 @@
+declare class BlockSource {
+    getDimension(): number;
+    getBlockId(x: number, y: number, z: number): number;
+    getBlockData(x: number, y: number, z: number): number;
+    setBlock(x: number, y: number, z: number, id: number, data: number): number;
+    getBlockEntity(x: number, y: number, z: number): NativeTileEntity;
+    getBiome(x: number, z: number): number;
+    setBiome(x: number, z: number, biomeID: number): void;
+    getBiomeTemperatureAt(x: number, y: number, z: number): number;
+    isChunkLoaded(chunkX: number, chunkZ: number): boolean;
+    getChunkState(chunkX: number, chunkZ: number): number;
+    canSeeSky(x: number, y: number, z: number): boolean;
+    getGrassColor(x: number, y: number, z: number): number;
+    spawnDroppedItem(x: number, y: number, z: number, id: number, count: number, data: number, extra: ItemExtraData): number;
+    fetchEntitiesInAABB(x1: number, y1: number, z1: number, x2: number, y2: number, z2: number, type: number, blacklist: boolean): number[];
+
+    static getDefaultForDimension(dimension: number): Nullable<BlockSource>;
+    static getDefaultForActor(entityUid: number): Nullable<BlockSource>;
+}
