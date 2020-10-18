5e79fe00592e0cc75d6f82e4b8d5998bcdf98f8b
Саня Череп <41489405+SDesya74@users.noreply.github.com>
2020-10-18 12:15:58 +0300

    Create SyncedNetworkData

---
+++ documentation/SyncedNetworkData.d.ts
@@ -0,0 +1,72 @@
+/**
+ * Class to work with values, synchronized between server and all clients
+ */
+declare class SyncedNetworkData {
+    /**
+     * @returns value by key
+     */
+    getInt(key: any, fallback?: number): number;
+    /**
+     * @returns value by key
+     */
+    getLong(key: any, fallback?: number): number;
+    /**
+     * @returns value by key
+     */
+    getFloat(key: any, fallback?: number): number;
+    /**
+     * @returns value by key
+     */
+    getDouble(key: any, fallback?: number): number;
+    /**
+     * @returns value by key
+     */
+    getString(key: any, fallback?: string): string;
+    /**
+     * @returns value by key
+     */
+    getBoolean(key: any, fallback?: boolean): boolean;
+    /**
+     * Sets value by key
+     */
+    putInt(key: any, value: number): void;
+    /**
+     * Sets value by key
+     */
+    putLong(key: any, value: number): void;
+    /**
+     * Sets value by key
+     */
+    putFloat(key: any, value: number): void;
+    /**
+     * Sets value by key
+     */
+    putDouble(key: any, value: number): void;
+    /**
+     * Sets value by key
+     */
+    putString(key: any, value: string): void;
+    /**
+     * Sets value by key
+     */
+    putBoolean(key: any, value: boolean): void;
+
+    /**
+     * Sends changed data values
+     */
+    sendChanges(): void;
+
+    /**
+     * Event that catches changes of any data values.
+     * @param networkData - SyncedNetworkData object where the changes had happened;
+     * @param isExternalStorage - 
+     * false, if change had happened by calling put from this object, 
+     * true, if it came by network from other connected data object.
+     */
+    addOnDataChangeListener(func: (networkData: SyncedNetworkData, isExternalChange: boolean) => void): void;
+
+    /**
+     * Adds data validator to the object
+     */
+    addVerifier(key: any, func: (key: any, newValue: any) => void): void;
+}
