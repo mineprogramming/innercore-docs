29fb5bfacb6ffc87c251ce3c1e2357e17978de2c
Саня Череп <41489405+SDesya74@users.noreply.github.com>
2020-10-18 12:16:15 +0300

    Create PlayerActor

---
+++ documentation/PlayerActor.d.ts
@@ -0,0 +1,135 @@
+
+/**
+ * Class to manipulate with separate players.
+ * It is temporary! It exists only 1 server tick!
+ */
+declare class PlayerActor {
+    constructor(playerUid: number);
+
+    /**
+     * @returns the id of dimension where player is.
+     */
+    getDimension(): number;
+
+    /**
+     * @returns player's gamemode.
+     */
+    getGameMode(): number;
+
+    /**
+     * Adds item to player's inventory
+     * @param dropRemainings if true, surplus will be dropped near player
+     */
+    addItemToInventory(id: number, count: number, data: number, extra?: ItemExtraData | null, dropRemainings?: boolean): void;
+
+    /**
+     * @returns inventory slot's contents.
+     */
+    getInventorySlot(slot: number): ItemInstance;
+
+    /**
+     * Sets inventory slot's contents.
+     */
+    setInventorySlot(slot: number, id: number, count: number, data: number, extra?: ItemExtraData | null): void;
+
+    /**
+     * @returns armor slot's contents.
+     */
+    getArmor(slot: number): ItemInstance;
+
+    /**
+     * Sets armor slot's contents.
+     */
+    setArmor(slot: number, id: number, count: number, data: number, extra?: ItemExtraData | null): void;
+
+    /**
+     * Sets respawn coords for the player.
+     */
+    setRespawnCoords(x: number, y: number, z: number): void;
+
+    /**
+     * Spawns exp on coords.
+     * @param value experience points value
+     */
+    spawnExpOrbs(x: number, y: number, z: number, value: number): void;
+
+    /**
+     * @returns whether the player is a valid entity.
+     */
+    isValid(): boolean;
+
+    /**
+     * @returns player's selected slot.
+     */
+    getSelectedSlot(): number;
+
+    /**
+     * Sets player's selected slot.
+     */
+    setSelectedSlot(slot: number): void;
+
+    /**
+     * @returns player's experience.
+     */
+    getExperience(): number;
+
+    /**
+     * Sets player's experience.
+     */
+    setExperience(value: number): void;
+
+    /**
+     * Add experience to player.
+     */
+    addExperience(amount: number): void;
+
+    /**
+     * @returns player's xp level.
+     */
+    getLevel(): number;
+
+    /**
+     * Sets player's xp level.
+     */
+    setLevel(level: number): void;
+
+    /**
+     * @returns player's exhaustion.
+     */
+    getExhaustion(): number;
+
+    /**
+     * Sets player's exhaustion.
+     */
+    setExhaustion(value: number): void;
+
+    /**
+     * @returns player's hunger.
+     */
+    getHunger(): number;
+
+    /**
+     * Sets player's hunger.
+     */
+    setHunger(value: number): void;
+
+    /**
+     * @returns player's saturation.
+     */
+    getSaturation(): number;
+
+    /**
+     * Sets player's saturation.
+     */
+    setSaturation(value: number): void;
+
+    /**
+     * @returns player's score.
+     */
+    getScore(): number;
+
+    /**
+     * Sets player's score.
+     */
+    setScore(value: number): void;
+}
