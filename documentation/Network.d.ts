d03c23f381c74d82f8fc194c7e2d7288f97cc1f9
Саня Череп <41489405+SDesya74@users.noreply.github.com>
2020-10-18 12:16:43 +0300

    Create Network

---
+++ documentation/Network.d.ts
@@ -0,0 +1,48 @@
+/**
+ * New module to work with client and server packets in multiplayer.
+ */
+declare namespace Network {
+    /**
+     * Event that is called when a client receives a packet with given name
+     * @param name name of the packet
+     */
+    function addClientPacket(name: string, func: (packetData: any) => void): void;
+
+    /**
+     * Event that is called when server receives a packet with the specified name from client
+     * @param name name of the packet
+     */
+    function addServerPacket(name: string, func: (client: any, data: any) => void): void;
+
+    /**
+     * Client class
+     */
+    class Client {
+        send(name: string, packetData: any): void;
+    }
+
+    /**
+     * Sends packet object with specified name to all clients
+     */
+    function sendToAllClients(name: string, packetData: any): void;
+
+    /**
+     * Sends packet object with the specified name from client to server
+     */
+    function sendToServer(name: string, packetData: any): void;
+
+    /**
+     * @returns Client object for player by player's entity id
+     */
+    function getClientForPlayer(player: number): Client;
+
+    /**
+     * Converts item or block id from server to local value
+     */
+    function serverToLocalId(id: string | number): number;
+
+    /**
+     * Converts item or block id from local to server value
+     */
+    function localToServerId(id: string | number): number;
+}
