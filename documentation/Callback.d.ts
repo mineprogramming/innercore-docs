e9c0fcbbdfeaadef296ac6fd96358c2e3961f249
vstannumdum <63312903+DMHYT@users.noreply.github.com>
2020-11-02 19:29:57 +0200

    Tyno

--- documentation/Callback.d.ts
+++ documentation/Callback.d.ts
@@ -16,7 +16,7 @@ declare namespace Callback {
     /**
      * Invokes callback with any name and up to 10 additional parameters. You
      * should not generally call pre-defined callbacks until you really need to 
-     * do so. If you want to trigger some event in your mode, use your own 
+     * do so. If you want to trigger some event in your mod, use your own 
      * callback names
      * @param name callback name
      */
