b7bfc3e7d92b9a1f48f94e5827c4e0d87225f4ec
Саня Череп <41489405+SDesya74@users.noreply.github.com>
2020-11-06 22:26:48 +0300

    Declare some methods of NetworkEntity

--- documentation/NetworkEntity.d.ts
+++ documentation/NetworkEntity.d.ts
@@ -2,5 +2,8 @@
  * Class that represents network entity of the block, currently is not learned
  */
 declare class NetworkEntity {
-
+	constructor(type: NetworkEntityType, context: any);
+	remove(): void;
+	send(name: string, data: any): void;
+	getClients(): ConnectedClientList;
 }
